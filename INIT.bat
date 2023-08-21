fee44516cad42de548d536eb75041421be3e6443
mohamed <mderkaoui10@gmail.com>
2023-08-21 12:43:08 +0200

    save date 2023-08-21 time 12:43:08,70

---
+++ INIT.bat
@@ -0,0 +1,7 @@
+echo "# k.page.oi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MohamedElderkaoui/k.page.oi.git
+git push -u origin main
---
+++ save.bat
@@ -0,0 +1,4 @@
+@REM save in git repository
+git add .
+git commit -m "save date %date% time %time%"
+git push -u origin main
